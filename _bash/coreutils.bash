52c0b5c2d435458dd49148439379397de66ba4d8
lucasb-eyer <lucasb.eyer.be@gmail.com>
2012-10-08 10:51:13 +0200

    Fix missing --color flags on mac os x.

--- _bash/coreutils.bash
+++ _bash/coreutils.bash
@@ -1,9 +1,13 @@
+
+# This is a crude check to see whether commands like ls and grep support the --color flag.
+HAS_COLORS=1 && command -v dircolors > /dev/null 2>&1 || HAS_COLORS=0
+
 # Add colors and line numbers
-alias grep='grep -n --color=auto'
+[ $HAS_COLORS -ne 0 ] && alias grep='grep -n --color=auto'
 
 # Add colors to ls
-alias ls='ls --color=auto'
+[ $HAS_COLORS -ne 0 ] && alias ls='ls --color=auto'
+[ $HAS_COLORS -ne 0 ] && eval $(dircolors -b)
 alias ll='ls -lh'
 alias la='ls -alh'
-eval $(dircolors -b)
 
