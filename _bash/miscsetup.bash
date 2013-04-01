4c9a51be3b0d4140dc57694fc1758ca56c46db3d
lucasb-eyer <lucasb.eyer.be@gmail.com>
2013-04-01 16:45:54 +0200

    Use .pythonrc only if it actually exists.

--- _bash/miscsetup.bash
+++ _bash/miscsetup.bash
@@ -23,4 +23,7 @@ if [ -d ~/go ]; then
 fi
 
 # Make the python interactive console a bit smarter.
-export PYTHONSTARTUP=~/.pythonrc.py
+if [-f ~/.pythonrc.py ]; then
+    export PYTHONSTARTUP=~/.pythonrc.py
+fi
+
