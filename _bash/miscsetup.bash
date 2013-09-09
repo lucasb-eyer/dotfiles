349e2150e3a5d5a67db3a61d21694c0f1ab4c4e8
lucasb-eyer <lucasb.eyer.be@gmail.com>
2013-09-09 17:35:05 +0200

    Path for local (~) installations.

--- _bash/miscsetup.bash
+++ _bash/miscsetup.bash
@@ -27,3 +27,9 @@ if [ -f ~/.pythonrc.py ]; then
     export PYTHONSTARTUP=~/.pythonrc.py
 fi
 
+# On many computers I am required to install things locally.
+if [ -d ~/inst ]; then
+    export PATH=~/inst/bin:$PATH
+    export LD_LIBRARY_PATH=~/inst/lib64:~/inst/lib:$LD_LIBRARY_PATH
+    export PYTHONPATH=~/inst/lib/python2.7/site-packages:$PYTHONPATH
+fi
