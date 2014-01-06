9dedab05f13377a916bae4161f9f7e0090cec6fd
lucasb-eyer <lucasb.eyer.be@gmail.com>
2014-01-06 18:39:40 +0100

    Matlab/awesome interaction fix.

--- _bash/bugfixes.bash
+++ _bash/bugfixes.bash
@@ -1,2 +1,6 @@
 # For when flashplayer sucks (bleeds through to other windows)
 VDPAU_NVIDIA_NO_OVERLAY=1
+
+#matlab fix for grey windows
+export MATLAB_JAVA=/usr/lib/jvm/java-6-openjdk-amd64/jre
+export _JAVA_AWT_WM_NONREPARENTING=1
