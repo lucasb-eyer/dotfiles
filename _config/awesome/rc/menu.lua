603989db3313549ef543770abf236b209b03b916
lucasb-eyer <lucasb.eyer.be@gmail.com>
2015-09-22 12:48:56 +0200

    Custom icons in awesomewm menu.
    For instance, if KDE isn't installed, the previous one fails to show a
    menu!

--- _config/awesome/rc/menu.lua
+++ _config/awesome/rc/menu.lua
@@ -1,25 +1,24 @@
+local keydoc = require("lib/keydoc")
 local lucasb = require("lib/lucasb")
 
 -- Create a laucher widget and a main menu
 myawesomemenu = {
-   { "manual", terminal .. " -e man awesome", "/usr/share/icons/oxygen/16x16/actions/help-contents.png" },
-   { "restart", awesome.restart, "/usr/share/icons/oxygen/16x16/actions/view-refresh.png" },
-   { "quit", awesome.quit, "/usr/share/icons/oxygen/16x16/actions/system-log-out.png" }
+   { "hotkeys", keydoc.display, "/home/beyer/.config/awesome/themes/hotkey.png" },
+   { "restart", awesome.restart, "/home/beyer/.config/awesome/themes/reboot.png" },
+   { "quit", awesome.quit, "/home/beyer/.config/awesome/themes/account-logout.png" }
 }
 
 powermenu = {
-    { "shutdown", "systemctl poweroff", "/usr/share/icons/oxygen/16x16/actions/system-shutdown.png" },
-    { "reboot", "systemctl reboot", "/usr/share/icons/oxygen/16x16/actions/system-reboot.png" },
-    { "suspend", lucasb.standby, "/usr/share/icons/oxygen/16x16/actions/system-suspend.png" },
-    { "hibernate", lucasb.hibernate, "/usr/share/icons/oxygen/16x16/actions/system-suspend-hibernate.png" },
-    { "lock", lucasb.lockscreen, "/usr/share/icons/oxygen/16x16/actions/system-lock-screen.png" },
-    { "lock & suspend", function() lucasb.lockscreen() lucasb.standby() end, "/usr/share/icons/oxygen/16x16/actions/system-lock-screen.png" },
-    { "lock & hibernate", function() lucasb.lockscreen() lucasb.hibernate() end, "/usr/share/icons/oxygen/16x16/actions/system-lock-screen.png" }
+    { "shutdown", "systemctl poweroff", "/home/beyer/.config/awesome/themes/poweroff.png" },
+    { "reboot", "systemctl reboot", "/home/beyer/.config/awesome/themes/reboot.png" },
+    { "suspend", lucasb.standby, "/home/beyer/.config/awesome/themes/suspend.png" },
+    { "lock", lucasb.lockscreen, "/home/beyer/.config/awesome/themes/lock.png" },
+    { "lock & suspend", function() lucasb.lockscreen() lucasb.standby() end, "/home/beyer/.config/awesome/themes/lock-suspend.png" },
 }
 
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "power", powermenu, "/usr/share/icons/oxygen/16x16/actions/quickopen.png" },
-                                    { "terminal", terminal, "/usr/share/icons/oxygen/16x16/apps/utilities-terminal.png" }
+mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, "/home/beyer/.config/awesome/themes/awesome.png" }, -- beautiful.awesome_icon },
+                                    { "power", powermenu, "/home/beyer/.config/awesome/themes/poweroff.png" },
+                                    { "terminal", terminal, "/home/beyer/.config/awesome/themes/terminal.png" }
                                   }
                         })
 
