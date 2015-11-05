1313528cb62b929585b09f5ad97432976e9ea584
lucasb-eyer <lucasb.eyer.be@gmail.com>
2015-11-05 10:13:10 +0100

    Update paths for local installs, cuda and cudnn

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -1,5 +1,18 @@
-set -x PATH ~/inst/bin $PATH
 set -x EDITOR vim
+
+# Add custom stuff to the path
+set -x PATH ~/inst/bin $PATH
+set -x LD_LIBRARY_PATH ~/inst/lib $LD_LIBRARY_PATH
+
+# Add cuda stuff to the path
+set -x PATH /usr/local/cuda-7.0/bin $PATH
+set -x LD_LIBRARY_PATH /usr/local/cuda-7.0/lib64 $LD_LIBRARY_PATH
+
+# Add cudnn stuff to the path
+set -x LD_LIBRARY_PATH ~/inst/cudnn3/lib64/ $LD_LIBRARY_PATH
+set -x LIBRARY_PATH ~/inst/cudnn3/lib64/ $LIBRARY_PATH
+set -x CPATH ~/inst/cudnn3/include/ $CPATH
+
 
 # Setup go if it exists in the home folder.
 if test -d ~/go
