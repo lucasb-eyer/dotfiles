3eaa113e155e433992c69e8d76150b7302090836
Lucas Beyer <lucas.beyer@kindred.ai>
2016-09-21 15:14:34 -0400

    Don't hardcode awesomewm icon location.

--- _config/awesome/rc/menu.lua
+++ _config/awesome/rc/menu.lua
@@ -1,26 +1,29 @@
 local keydoc = require("lib/keydoc")
 local lucasb = require("lib/lucasb")
 
+local icondir = awful.util.getdir("config") .. "/themes/"
+
 -- Create a laucher widget and a main menu
 myawesomemenu = {
-   { "hotkeys", keydoc.display, "/home/beyer/.config/awesome/themes/hotkey.png" },
-   { "restart", awesome.restart, "/home/beyer/.config/awesome/themes/reboot.png" },
-   { "quit", awesome.quit, "/home/beyer/.config/awesome/themes/account-logout.png" }
+   { "&hotkeys", keydoc.display, icondir .. "hotkey.png" },
+   { "&restart", awesome.restart, icondir .. "reboot.png" },
+   { "&quit", awesome.quit, icondir .. "account-logout.png" }
 }
 
 powermenu = {
-    { "shutdown", "systemctl poweroff", "/home/beyer/.config/awesome/themes/poweroff.png" },
-    { "reboot", "systemctl reboot", "/home/beyer/.config/awesome/themes/reboot.png" },
-    { "suspend", lucasb.standby, "/home/beyer/.config/awesome/themes/suspend.png" },
-    { "lock", lucasb.lockscreen, "/home/beyer/.config/awesome/themes/lock.png" },
-    { "lock & suspend", function() lucasb.lockscreen() lucasb.standby() end, "/home/beyer/.config/awesome/themes/lock-suspend.png" },
+    { "&shutdown", "systemctl poweroff", icondir .. "poweroff.png" },
+    { "&reboot", "systemctl reboot", icondir .. "reboot.png" },
+    { "s&uspend", lucasb.standby, icondir .. "suspend.png" },
+    { "&lock", lucasb.lockscreen, icondir .. "lock.png" },
+    { "l&ock & suspend", function() lucasb.lockscreen() lucasb.standby() end, icondir .. "lock-suspend.png" },
 }
 
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, "/home/beyer/.config/awesome/themes/awesome.png" }, -- beautiful.awesome_icon },
-                                    { "power", powermenu, "/home/beyer/.config/awesome/themes/poweroff.png" },
-                                    { "terminal", terminal, "/home/beyer/.config/awesome/themes/terminal.png" }
-                                  }
-                        })
+mymainmenu = awful.menu.new({ items = {
+    { "&awesome", myawesomemenu, icondir .. "awesome.png" }, -- beautiful.awesome_icon },
+    { "&power", powermenu, icondir .. "poweroff.png" },
+    { "&terminal", terminal, icondir .. "terminal.png" }
+  }
+})
 
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
                                      menu = mymainmenu })
